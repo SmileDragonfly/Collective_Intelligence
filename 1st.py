c92f09e5172876def37c56e67eeadbc0a2509b32
DFLY\DFLY <dat.doantrongk57@gmail.com>
2018-12-24 00:14:07 +0700

    built-in library

--- 1st.py
+++ 1st.py
@@ -21,12 +21,20 @@
 
 # print("Hello from Python!")
 # print("Your Python version is: " + sys.version)
-released = {
-		"iphone" : 2007,
-		"iphone 3G" : 2008,
-		"iphone 3GS" : 2009,
-		"iphone 4" : 2010,
-		"iphone 4S" : 2011,
-		"iphone 5" : 2012
-	}
-print (released["iphone"])
+# released = {
+# 		"iphone" : 2007,
+# 		"iphone 3G" : 2008,
+# 		"iphone 3GS" : 2009,
+# 		"iphone 4" : 2010,
+# 		"iphone 4S" : 2011,
+# 		"iphone 5" : 2012
+# 	}
+# print (released["iphone"])
+# import pydelicious
+# pydelicious.get_popular(tag="programming")
+from math import sqrt
+print sqrt(3)
+# get all built-in library
+import sys
+a=sys.builtin_module_names
+print a
